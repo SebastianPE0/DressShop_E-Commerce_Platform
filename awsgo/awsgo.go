66b138f8946217f01714fe6e3902ce1ea45ba1ba
SebastianPE0 <seb.perez02@gmail.com>
2025-01-20 20:58:33 -0500

    fix: :bug: Fix bugs on awsgo

--- awsgo/awsgo.go
+++ awsgo/awsgo.go
@@ -1,23 +1,26 @@
-awsgo.go
-	package awsgo
-	import{
-		“context”
-		“go get github.com/aws/aws-sdk-go-v2/aws”
-		“go get github.com/aws/aws-sdk-go-v2/config”
-	}
-Var Ctx context.Context
-Var Cfg aws.Config.
-Var err error
+package awsgo
 
-Func InicializoAWS{
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+)
+
+var Ctx context.Context
+var Cfg aws.Config
+var err error
+
+func InicializoAWS() {
 	Ctx = context.TODO()
-	Cfg, err = config.LoadDefaultConfig(Ctx, config.WithDefaultRegion(“us-east-1”))
+	Cfg, err = config.LoadDefaultConfig(Ctx, config.WithDefaultRegion("us-east-1"))
 
-	If err !) nil{
-		panic(“Error al cargar la configuración .aws/config “+err.Error())
+	if err != nil {
+		panic("Error al cargar la configuración .aws/config " + err.Error())
 	}
 }
-go get github.com/aws/aws-lambda-go/lamda
+
+/*go get github.com/aws/aws-lambda-go/lamda
 go get github.com/aws/aws-lambda-go/events
 go get github.com/aws/aws-sdk-go-v2/aws
-go get github.com/aws/aws-sdk-go-v2/config
+go get github.com/aws/aws-sdk-go-v2/config*/
