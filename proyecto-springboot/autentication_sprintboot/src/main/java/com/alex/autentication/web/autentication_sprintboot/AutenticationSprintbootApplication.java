fe1861cf4061129541f230e7aeb72345b4c66341
Alexander2198 <alexandercajamarca7777@gmail.com>
2025-01-20 22:28:15 -0500

    feat: :sparkles: autentication sprintboot

---
+++ proyecto-springboot/autentication_sprintboot/src/main/java/com/alex/autentication/web/autentication_sprintboot/AutenticationSprintbootApplication.java
@@ -0,0 +1,13 @@
+package com.alex.autentication.web.autentication_sprintboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticationSprintbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticationSprintbootApplication.class, args);
+	}
+
+}
