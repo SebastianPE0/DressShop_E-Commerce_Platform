0df562428eceff8d0a7e84d3f5e05c461994da9b
SebastianPE0 <107443362+SebastianPE0@users.noreply.github.com>
2025-01-21 21:16:58 -0500

    Revert "Add configuration of 2 microservices"

---
+++ BackEnd/Products/CreateProduct/internal/database/mongo.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectMongoDB(uri string, dbName string) (*mongo.Database, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		return nil, err
+	}
+
+	return client.Database(dbName), nil
+}
