daf8ea795fc8c560b0cbb4569775257fe66cc4c0
SebastianPE0 <seb.perez02@gmail.com>
2025-01-30 12:27:32 -0500

    fix:

--- BackEnd/Categories/CreateCategory/src/routes/categoryRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const categoryController = require('../controllers/categoryController');
-
-router.post('/create', categoryController.createCategory);
-
-module.exports = router;
--- BackEnd/Products/CreateProduct/models/products.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Product struct {
-	ID    string  `json:"id,omitempty" bson:"_id,omitempty"`
-	Name  string  `json:"name" bson:"name"`
-	Price float64 `json:"price" bson:"price"`
-}
