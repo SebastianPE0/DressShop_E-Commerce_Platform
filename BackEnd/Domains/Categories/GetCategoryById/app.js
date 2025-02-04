4a3d2341359892119ce1ea4295f2c8e01602dc94
SebastianPE0 <seb.perez02@gmail.com>
2025-02-03 23:41:34 -0500

    fix: :construction: Change folder of app.js and server.js

--- BackEnd/Domains/Categories/GetCategoryById/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const categoryRoutes = require('./src/routes/categoryRoutes');
-
-const app = express();
-
-app.use(express.json()); 
-app.use('/category', categoryRoutes); 
-
-module.exports = app;
