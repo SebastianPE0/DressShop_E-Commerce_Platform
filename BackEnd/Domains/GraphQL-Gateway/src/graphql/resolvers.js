475037791663e4c9cc5147cea7a49ee7359b9852
SebastianPE0 <seb.perez02@gmail.com>
2025-02-06 13:01:18 -0500

    fix: :bug: Fix typing bug of url to get-product-by-category

--- BackEnd/Domains/GraphQL-Gateway/src/graphql/resolvers.js
+++ BackEnd/Domains/GraphQL-Gateway/src/graphql/resolvers.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
-require('dotenv').config(); // Importamos dotenv para cargar las variables de entorno
+require('dotenv').config();
 
-const CATEGORY_SERVICE_URL = process.env.CATEGORY_SERVICE_URL ;
-const PRODUCT_SERVICE_URL = process.env.PRODUCT_SERVICE_URL ;
+const CATEGORY_SERVICE_URL = process.env.CATEGORY_SERVICE_URL;
+const PRODUCT_SERVICE_URL = process.env.PRODUCT_SERVICE_URL;
 
 const resolvers = {
   Query: {
@@ -16,17 +16,18 @@ const resolvers = {
     },
     getProductsByCategory: async (_, { categoryId }) => {
       try {
-        console.log(`Requesting products from: ${PRODUCT_SERVICE_URL}/products/by-category?category_id=${categoryId}`);
-        
-        const response = await axios.get(`${PRODUCT_SERVICE_URL}/products/by-category?category_id=${categoryId}`);
+        const url = `${PRODUCT_SERVICE_URL}?category_id=${categoryId}`;
+        console.log(`🔍 Sending request to: ${url}`);
+
+        const response = await axios.get(url);
         console.log("Response from product service:", response.data);
-        
+
         return response.data;
       } catch (error) {
         console.error("Error fetching products by category:", error.response ? error.response.data : error.message);
         throw new Error(error.response ? error.response.data : error.message);
       }
-    }
+    },
   },
 };
 
