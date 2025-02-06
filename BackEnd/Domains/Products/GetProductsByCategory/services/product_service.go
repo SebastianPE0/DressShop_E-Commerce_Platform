251cbf67ba700ed36af09ce351c198583601e615
SebastianPE0 <seb.perez02@gmail.com>
2025-02-06 11:45:26 -0500

    feat: :wrench: Add configuration of GetProductsByCategory Microservice

---
+++ BackEnd/Domains/Products/GetProductsByCategory/services/product_service.go
@@ -0,0 +1,18 @@
+package services
+
+import (
+	"github.com/SebastianPE0/DressShop_E-Commerce_Platform/BackEnd/Products/GetProductsByCategory/models"
+	"github.com/SebastianPE0/DressShop_E-Commerce_Platform/BackEnd/Products/GetProductsByCategory/repositories"
+)
+
+type ProductService struct {
+	Repo *repositories.ProductRepository
+}
+
+func NewProductService(repo *repositories.ProductRepository) *ProductService {
+	return &ProductService{Repo: repo}
+}
+
+func (s *ProductService) GetProductsByCategory(categoryID string) ([]models.Product, error) {
+	return s.Repo.GetProductsByCategory(categoryID)
+}
