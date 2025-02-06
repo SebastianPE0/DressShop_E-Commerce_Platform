ba63b51e3c71e44f90759990806eaf286ddfb7e9
SebastianPE0 <seb.perez02@gmail.com>
2025-02-06 15:27:45 -0500

    fix: :wrench: update route

--- BackEnd/Domains/GraphQL-Gateway/src/graphql/resolvers.js
+++ BackEnd/Domains/GraphQL-Gateway/src/graphql/resolvers.js
@@ -8,10 +8,16 @@ const resolvers = {
   Query: {
     getCategoryById: async (_, { id }) => {
       try {
-        console.log(`Requesting category from: ${CATEGORY_SERVICE_URL}/${id}`);
-        const response = await axios.get(`${CATEGORY_SERVICE_URL}/${id}`);
-        console.log(`Response from category service:`, response.data);
-        return response.data;
+        console.log(`Requesting category from: ${CATEGORY_SERVICE_URL}/category/${id}`);
+        const response = await axios.get(`${CATEGORY_SERVICE_URL}/category/${id}`);
+
+        const category = response.data;
+        console.log(`Response from category service:`, category);
+
+        return {
+          id: category._id,  // Convertimos _id a id
+          name: category.name
+        };
       } catch (error) {
         console.error("Error fetching category by ID:", error.response ? error.response.data : error.message);
         throw new Error("Failed to fetch category.");
