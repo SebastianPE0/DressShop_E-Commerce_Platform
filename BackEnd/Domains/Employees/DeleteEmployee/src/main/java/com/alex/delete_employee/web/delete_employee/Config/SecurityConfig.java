c2539fc5045131ae3f58658ececcf1d6d62e6639
Alexander2198 <alexandercajamarca7777@gmail.com>
2025-02-07 13:55:58 -0500

    feat: :zap: config

--- BackEnd/Domains/Employees/DeleteEmployee/src/main/java/com/alex/delete_employee/web/delete_employee/Config/SecurityConfig.java
+++ BackEnd/Domains/Employees/DeleteEmployee/src/main/java/com/alex/delete_employee/web/delete_employee/Config/SecurityConfig.java
@@ -2,16 +2,17 @@ package com.alex.delete_employee.web.delete_employee.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.CorsConfigurationSource;
-
+import org.springframework.security.config.http.SessionCreationPolicy;
 import java.util.List;
 
 @Configuration
