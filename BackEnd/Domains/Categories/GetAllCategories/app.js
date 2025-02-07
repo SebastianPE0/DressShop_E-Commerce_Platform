2d6767b2327f257f038c2f57f0db2d0aed13e387
SebastianPE0 <seb.perez02@gmail.com>
2025-02-07 17:40:12 -0500

    test:

--- BackEnd/Domains/Categories/GetAllCategories/app.js
+++ BackEnd/Domains/Categories/GetAllCategories/app.js
@@ -1,7 +1,16 @@
 const express = require("express");
+//AÑADIDO
+const cors = require("cors"); // Importar CORS
+
 const categoryRoutes = require("./src/routes/categoryRoutes");
 
 const app = express();
+//METODO AÑADIDO
+app.use(cors({
+    origin: "http://ec2-54-242-43-40.compute-1.amazonaws.com", // Permitir solo el frontend en EC2
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use(express.json());
 app.use("/categories", categoryRoutes);
