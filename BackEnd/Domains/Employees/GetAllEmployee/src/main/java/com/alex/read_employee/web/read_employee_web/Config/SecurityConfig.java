6cbf0b8433cf714cf8b257e9680e0fef58560e6c
Alexander2198 <alexandercajamarca7777@gmail.com>
2025-02-07 21:38:16 -0500

    feat: :zap: config total

--- BackEnd/Domains/Employees/GetAllEmployee/src/main/java/com/alex/read_employee/web/read_employee_web/Config/SecurityConfig.java
+++ BackEnd/Domains/Employees/GetAllEmployee/src/main/java/com/alex/read_employee/web/read_employee_web/Config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
         config.setAllowCredentials(true);
         config.setAllowedOrigins(List.of(
             "http://localhost:5173",
-            "ec2-54-211-104-82.compute-1.amazonaws.com" // Permitir frontend en AWS
+            "http://ec2-54-211-104-82.compute-1.amazonaws.com" // Permitir frontend en AWS
         ));
         config.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
