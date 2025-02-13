32c469ff209f5e1f3feca71e07aec241a9fec565
Alexander2198 <alexandercajamarca7777@gmail.com>
2025-02-12 22:59:04 -0500

    feat: :sparkles: config category

--- FrontEnd/employee-app/src/components/Login.jsx
+++ FrontEnd/employee-app/src/components/Login.jsx
@@ -12,7 +12,7 @@ const Login = ({ onLogin }) => {
         try {
             await EmployeeService.login(email, password);
             onLogin();  // Cambia el estado de autenticación en App.jsx
-            navigate("/categories"); // Redirige a la listas de empleados
+            navigate("/"); // Redirige a la listas de empleados
         } catch (error) {
             alert("Error en el inicio de sesión");
         }
