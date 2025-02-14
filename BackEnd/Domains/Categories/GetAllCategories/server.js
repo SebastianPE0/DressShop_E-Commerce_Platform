00eb0a2250f02e6414c37009eb904f32e6706795
SebastianPE0 <seb.perez02@gmail.com>
2025-02-14 10:35:17 -0500

    feat: :rocket: test deploy get all category

--- BackEnd/Domains/Categories/GetAllCategories/server.js
+++ BackEnd/Domains/Categories/GetAllCategories/server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 const connectDB = require("./src/config/db");
 const config = require("./src/config/env");
 
-// Connect to MongoDB and Up the server (TEST )
+// Connect to MongoDB and Up the server (TEST 2)
 connectDB();
 app.listen(config.appPort, () => {
   console.log(` GetAllCategories service running on port ${config.appPort}`);
