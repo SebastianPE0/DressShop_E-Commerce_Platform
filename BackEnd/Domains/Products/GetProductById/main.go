d67e4cf31b85114f446949ea96824b62ded9b7cc
SebastianPE0 <seb.perez02@gmail.com>
2025-02-13 20:45:53 -0500

    test: :rocket: Test deploy getbyid-product

--- BackEnd/Domains/Products/GetProductById/main.go
+++ BackEnd/Domains/Products/GetProductById/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/SebastianPE0/DressShop_E-Commerce_Platform/BackEnd/Products/GetProductById/config"
 	"github.com/SebastianPE0/DressShop_E-Commerce_Platform/BackEnd/Products/GetProductById/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -18,14 +19,24 @@ func main() {
 	// Crear instancia de Gin
 	router := gin.Default()
 
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://3.214.134.68"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Content-Type", "Authorization"},
+		AllowCredentials: true,
+	}))
+
 	router.Use(config.AuthMiddleware())
+	// Configurar el servidor con Gin
+
+	// Aplicar CORS
 
 	// Configurar rutas
 	routes.SetupRoutes(router)
 
 	// Iniciar el servidor
 	log.Println("Servidor corriendo en el puerto 80")
-	if err := router.Run(":80"); err != nil {
+	if err := router.Run(":6002"); err != nil {
 		log.Fatal("Error al iniciar el servidor:", err)
 	}
 
