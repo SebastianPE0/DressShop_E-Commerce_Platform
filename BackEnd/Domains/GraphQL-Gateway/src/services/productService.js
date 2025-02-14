4ae793f00fec31bbcc5ea4e93fa0238d15c4f4bd
SebastianPE0 <seb.perez02@gmail.com>
2025-02-13 19:25:06 -0500

    fix: :sparkles: Fix dns for instances

--- BackEnd/Domains/GraphQL-Gateway/src/services/productService.js
+++ BackEnd/Domains/GraphQL-Gateway/src/services/productService.js
@@ -1,12 +1,12 @@
 const axios = require("axios");
 
 const env = require('../config/env');
-
+const PRODUCT_SERVICE_URL="http://ec2-44-204-88-189.compute-1.amazonaws.com:3001"
 
 async function getProductsByCategory(categoryId) {
   try {
     console.log(`🔍 Consultando productos con categoryId: ${categoryId}`);
-    const response = await axios.get(`${process.env.PRODUCT_SERVICE_URL}/products/category/${categoryId}`);
+    const response = await axios.get(`http://ec2-44-204-88-189.compute-1.amazonaws.com:3001/products/category/${categoryId}`);
     return response.data;
   } catch (error) {
     console.error("❌ Error en getProductsByCategory:", error.response?.data || error.message);
