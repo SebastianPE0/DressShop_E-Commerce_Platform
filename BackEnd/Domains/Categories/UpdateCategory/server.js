15a9d33aa23f4665acb9b90efeeba6a2fc90e7f1
SebastianPE0 <seb.perez02@gmail.com>
2025-02-14 10:11:16 -0500

    feat: :sparkles: Add general deploy for git actions

--- BackEnd/Domains/Categories/UpdateCategory/server.js
+++ BackEnd/Domains/Categories/UpdateCategory/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = require("./app");
 
-const PORT = process.env.PORT || 80;
+const PORT = 3002;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
